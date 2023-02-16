59853274a34ebb3239707ae29c06bc157a899ec2
max <bozzhik@ya.ru>
2023-02-16 19:19:26 +0300

    other fix stack and build
    I don’t really remember what I changed here, but the tailwind worked and I tried it. this is unreal cool and fast 0.0

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,20 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      <div className="flex mx-auto justify-center text-3xl mt-[10vw]">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="./vite.svg" className="logo" alt="Vite logo" />
+          <img src="./public/vite.svg" className="h-[6em] will-change-[filter] transition-[filter] duration-300 p-[1.5em] logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="h-[6em] will-change-[filter] transition-[filter] duration-300 p-[1.5em] logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <a className="font-medium text-white hover:underline hover:text-violet-400 text-7xl" target={'_blank'} href="https://github.com/bozzzhik/vite_react_tailwindcss-stack">
+        Vite + React
+      </a>
+      <button className="mt-6 rounded border-transparent border-2 bg-[#1a1a1a] text-white text-xl font-medium cursor-pointer transition duration-500 ease-out px-3 py-1 grid place-items-center m-auto" onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </button>
     </div>
   )
 }
